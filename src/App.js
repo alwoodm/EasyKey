7cf4537dca78ac069dd6a7f0912f4a7aa9b97521
alwoodm <110826071+alwoodm@users.noreply.github.com>
2025-04-11 12:56:21 +0200

    Add Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="text-center">
+      <header className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-white">
+        <img src={logo} className="App-logo h-40 pointer-events-none" alt="logo" />
+        <p className="text-xl">
+          Edit <code className="font-mono">src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-[#61dafb] mt-4"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
